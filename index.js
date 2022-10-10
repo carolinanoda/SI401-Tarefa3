f736738ee31351f85ab57f9ed693cb3ca11eebd2
Carolina Noda <carolinanoda2001@gmail.com>
2022-10-10 19:29:36 -0300

    att 2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let count = 0;
-vez = count % 2;
 
 function onClick() {
     player1 = document.getElementById("firstPlayer").value;
@@ -21,12 +20,11 @@ function onClickStart() {
 function onClickPos1() {
     vez = count % 2;
     if(vez == 0) {
-        document.getElementById('pos1p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos1p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos1p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos1p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -35,12 +33,12 @@ function onClickPos1() {
 function onClickPos2() {
     vez = count % 2;
     if(vez == 0) {
-        document.getElementById('pos2p').innerHTML = "X<span class='element;'>";
+        p2 = 'X';
+        document.getElementById('pos2p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos2p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos2p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -49,12 +47,12 @@ function onClickPos2() {
 function onClickPos3() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos3p').innerHTML = "X<span class='element;'>";
+        p3 = 'X';
+        document.getElementById('pos3p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos3p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos3p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -63,12 +61,11 @@ function onClickPos3() {
 function onClickPos4() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos4p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos4p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos4p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos4p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -77,12 +74,11 @@ function onClickPos4() {
 function onClickPos5() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos5p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos5p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos5p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos5p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -91,12 +87,11 @@ function onClickPos5() {
 function onClickPos6() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos6p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos6p').innerHTML = "X";
         count++;
-        XisChecked2 = true;
     }
     else {
-        document.getElementById('pos6p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos6p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -105,12 +100,11 @@ function onClickPos6() {
 function onClickPos7() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos7p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos7p').innerHTML = "X";
         count++;
-        XisChecked3 = true;
     }
     else {
-        document.getElementById('pos7p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos7p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -119,12 +113,11 @@ function onClickPos7() {
 function onClickPos8() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos8p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos8p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos8p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos8p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
@@ -133,13 +126,18 @@ function onClickPos8() {
 function onClickPos9() {
     vez = count % 2;
     if(count % 2 == 0) {
-        document.getElementById('pos9p').innerHTML = "X<span class='element;'>";
+        document.getElementById('pos9p').innerHTML = "X";
         count++;
-        XisChecked1 = true;
     }
     else {
-        document.getElementById('pos9p').innerHTML = "O<span class='element;'>";
+        document.getElementById('pos9p').innerHTML = "O";
         count++;
     }
     document.getElementById('turn').innerHTML = "Vez de: <span style='color: #ff004d; text-transform: uppercase;'>" + players[vez];
+}
+
+clickOnce.addEventListener("click", myfunc());
+
+function myfunc() {
+    
 }
